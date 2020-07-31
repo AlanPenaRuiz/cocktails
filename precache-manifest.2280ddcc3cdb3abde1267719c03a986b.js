b9ce6e79862831ac9e12dfffb3ab94d700aacfc8
Alanhawk <alalanruiz@gmail.com>
2020-07-31 20:28:36 +0200

    Updates

---
+++ precache-manifest.2280ddcc3cdb3abde1267719c03a986b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596ec5665d6fba3a33059bab0cdc5ccd",
+    "url": "/cocktails/index.html"
+  },
+  {
+    "revision": "fcc9ed5a88359a86070a",
+    "url": "/cocktails/static/css/main.05aaf723.chunk.css"
+  },
+  {
+    "revision": "17c6a8a1264e32ea724a",
+    "url": "/cocktails/static/js/2.44612e05.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/cocktails/static/js/2.44612e05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc9ed5a88359a86070a",
+    "url": "/cocktails/static/js/main.93b18351.chunk.js"
+  },
+  {
+    "revision": "5216c598e7f51d9b5448",
+    "url": "/cocktails/static/js/runtime-main.3d73051c.js"
+  }
+]);
